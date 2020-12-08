4c5fffb6883a92ab4a46d89ce4b43344d673d5e1
dai954 <da0606ha@gmail.com>
2020-12-08 21:37:34 +0900

    Dssample_update1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,6 @@ class OrdersController < ApplicationController
 
   def edit
     @order = Order.find(params[:id])
- 
-    # @products = @user.products
     @product1 = Product.find(@order.product1)
     @product2 = Product.find(@order.product2)
   end
@@ -13,6 +11,11 @@ class OrdersController < ApplicationController
   def update
     order = Order.find(params[:id])
     order.update(order_params)
-    redirect_to order_path(order.id)
+    redirect_to root_path
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:product1, :number_of_product1a, :number_of_product1b, :product2, :number_of_product2a, :number_of_product2b)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'customers#index'
+  root to: 'orders#index'
   resources :users, only: :show
-  resources :orders, only: [:index, :edit]
+  resources :orders, only: [:index, :edit, :update]
   resources :customers, only: :index
 end
